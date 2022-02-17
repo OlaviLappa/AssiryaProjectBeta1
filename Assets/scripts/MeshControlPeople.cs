2ea6b4c0522a1c52dc2bc72f4bc27123e0f71ccb
OlaviLappa <tomas.shellenberg@mail.ru>
2022-02-17 21:02:50 +0300

    First commit

---
+++ Assets/scripts/MeshControlPeople.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MeshControlPeople : MonoBehaviour
+{
+    private Camera mainCamera;
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
