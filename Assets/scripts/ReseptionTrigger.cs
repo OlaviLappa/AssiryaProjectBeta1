20a4b5d7bd0f2df171e313e1e531bdb1b15bb896
BagmanG <zagr.gadel@gmail.com>
2022-02-19 15:02:56 +0300

    19.02.2022 - update2
    - cars
    - waypoints

---
+++ Assets/scripts/ReseptionTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReseptionTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Application.OpenURL("http://assyrian.beget.tech/");
+        }
+    }
+}
